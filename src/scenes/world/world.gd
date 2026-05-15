07520d129a52535897b5ca5007a8ed83a8a47ce8
Fred-el-jolo (Claude assistant) <guillaume.frederic@gmail.com>
2026-05-15 18:00:29 +0000

    feat: manual LAND button on proximity, auto-land on beacon arrival

--- src/scenes/world/world.gd
+++ src/scenes/world/world.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var landing_screen: LandingScreen = $LandingScreen
 
 var _pois: Array[PointOfInterest] = []
+var _poi_in_range: PointOfInterest = null
 
 func _ready() -> void:
 	for child in $POIs.get_children():
@@ -17,6 +18,7 @@ func _ready() -> void:
 	ShipInput.ship = ship
 	hud.connect_to_ship(ship)
 	hud.connect_to_world(ship, _pois)
+	hud.land_requested.connect(_on_land_requested)
 	BeaconSystem.register(ship, _pois)
 	BeaconSystem.beacon_activated.connect(func(): hud.show_beacon_active(true))
 	BeaconSystem.beacon_deactivated.connect(func(): hud.show_beacon_active(false))
@@ -26,8 +28,18 @@ func _process(_delta: float) -> void:
 		poi.check_landing_proximity(ship.position, ship.z_depth)
 
 func _on_landing_zone_entered(poi: PointOfInterest) -> void:
-	hud.show_landing_prompt(true)
-	landing_screen.show_for(poi, ship)
+	_poi_in_range = poi
+	if BeaconSystem.active:
+		landing_screen.show_for(poi, ship)
+	else:
+		hud.show_land_button(true)
 
 func _on_landing_zone_exited(_poi: PointOfInterest) -> void:
-	hud.show_landing_prompt(false)
+	_poi_in_range = null
+	hud.show_land_button(false)
+
+func _on_land_requested() -> void:
+	if _poi_in_range == null:
+		return
+	landing_screen.show_for(_poi_in_range, ship)
+	hud.show_land_button(false)
